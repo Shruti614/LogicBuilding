23479227ec31cee98f5fa9b39960240d4d6fb755
ShrutiGuldagad <104832642+Shruti614@users.noreply.github.com>
2022-06-26 12:41:25 +0530

    Add files via upload

--- A5/Q1.c
+++ A5/Q1.c
@@ -1,1 +1,38 @@
+/*Write a program which accept number from user and display its digits in reverse order.
+Input: 2395             1018            -1018           9000
+Output: 5               8               8               0
+        9               1               1               0
+        3               0               0               0
+        2               1               1               9
+*/
 
+#include<stdio.h>
+
+void DisplayDigit(int iNo)
+{
+    int iDigit=0;
+
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+
+    while(iNo>0)
+    {
+        iDigit=iNo%10;
+        printf("%d\n",iDigit);
+        iNo=iNo/10;
+    }
+}
+
+int main()
+{
+    int iValue=0;
+
+    printf("Enter number:\n");
+    scanf("%d",&iValue);
+
+    DisplayDigit(iValue);
+
+    return 0;
+}
